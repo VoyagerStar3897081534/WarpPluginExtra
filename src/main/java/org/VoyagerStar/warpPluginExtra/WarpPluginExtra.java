e346c4fb88a1e0238e1cf8f9f9c5bfb405025fbc
VoyagerStar3897081534 <3897081534@qq.com>
2026-03-29 17:22:12 +0800

    init commit

---
+++ src/main/java/org/VoyagerStar/warpPluginExtra/WarpPluginExtra.java
@@ -0,0 +1,17 @@
+package org.VoyagerStar.warpPluginExtra;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class WarpPluginExtra extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
